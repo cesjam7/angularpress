e95e39edfb5b58b17091d0485c160c96336acce5
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2014-12-20 10:41:37 -0500

    buscador

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
-    <div class="container" ng-controller="MyController">
- 
+    <div class="container">
+      <br>
+      <input type="text" placeholder="Buscar en el sitio" class="form-control" ng-model="buscar">
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-md-4" ng-repeat="data in datos">
+        <div class="col-md-4" ng-repeat="data in datos | filter: buscar">
           <h2>{{data.title}}</h2>
           <div ng-bind-html="data.content | limitTo:250"></div>
         </div>
